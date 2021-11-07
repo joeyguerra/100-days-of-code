bea02ecf44a0629747e17b8890c0841808c22008
Joey Guerra <joey@joeyguerra.com>
2021-11-07 15:22:43 -0600

    initial commit

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 git init
-git remote add origin git@github.com:joeyguerra/100-days-of-code.git
+git remote add origin $1
 git add .
 git commit -m "initial commit"
 git push -u origin main
